02f8ac56954250e05d3db8b7d40d7c63e9131fb5
Hanna Arpiainen <hanna.k.arpiainen@helsinki.fi>
2014-02-26 09:12:38 +0200

    started model penguin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pingu::Application.routes.draw do
+  resources :penguins
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
