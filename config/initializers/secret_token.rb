8c9cfcf3c828a584216ac1c194dbce78a9aaad9c
Hanna Arpiainen <hanna.k.arpiainen@helsinki.fi>
2014-02-26 07:25:17 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pingu::Application.config.secret_key_base = '6771cea38988ec5db252b6aca83a90efb1d13e9b4541291d0f8c13a1331b8450d3099e574b1ef3e511d1b22c54b97629e55bdb55e90afa62e8e0ca30a5e87ed1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pingu::Application.config.session_store :cookie_store, key: '_pingu_session'
